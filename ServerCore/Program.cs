4b936bb388c7921bc1bc4feaf5d99a2f257f3d8b
joonsooan <joonsooan@gmail.com>
2024-10-13 19:25:57 +0900

    Generating Threads

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static void MainThread(object state)
+        {
+            for ( int i = 0; i < 5; i++ )
+            { 
+                Console.WriteLine("Hello Thread");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadPool.SetMinThreads(1, 1); // 최소 쓰레드: 1개
+            ThreadPool.SetMaxThreads(5, 5); // 최대 쓰레드: 5개
+
+            for (int i = 0; i < 5; i++)
+            {
+                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
+                t.Start();
+            }
+
+            //for ( int i = 0; i < 4; i++) // i < 5 이면 쓰레드 5개를 모두 사용, ThreadPool.QueueUserWorkItem(MainThread)이 실행 안됨
+            //{
+            //    ThreadPool.QueueUserWorkItem((obj) => { while (true) { } }); // 무한 루프를 돌림
+            //}
+
+            ThreadPool.QueueUserWorkItem(MainThread);
+
+            //Thread t = new Thread(MainThread);
+            //t.Name = "Test Thread";
+            //t.IsBackground = true;
+            //t.Start();
+
+            //Console.WriteLine("Waiting for Thread");
+
+            //t.Join();
+            //Console.WriteLine("Hello World");
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
