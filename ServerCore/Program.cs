1eb19e4bfb2b3d144741289515396d3aeb2e74b7
joonsooan <joonsooan@gmail.com>
2024-10-19 22:19:20 +0900

    MemoryBarrier 2

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -2,44 +2,29 @@
 {
     internal class Program
     {
-        private static int x = 0;
-        private static int y = 0;
-        private static int r1 = 0;
-        private static int r2 = 0;
+        private int _answer;
+        private bool _complete;
 
-        private static void Thread_1()
+        private void A()
         {
-            y = 1; // Store y
-            Thread.MemoryBarrier();
-            r1 = x; // Load x
+            _answer = 123;
+            Thread.MemoryBarrier(); // Barrier 1
+            _complete = true;
+            Thread.MemoryBarrier(); // Barrier 2
         }
 
-        private static void Thread_2()
+        private void B()
         {
-            x = 1; // Store x
-            Thread.MemoryBarrier();
-            r2 = y; // Load y
+            Thread.MemoryBarrier(); // Barrier 3
+            if (_complete)
+            {
+                Thread.MemoryBarrier(); // Barrier 4
+                Console.WriteLine(_answer);
+            }
         }
 
         private static void Main(string[] args)
         {
-            int count = 0;
-            while (true)
-            {
-                count++;
-                x = y = r1 = r2 = 0;
-
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
-                t1.Start();
-                t2.Start();
-
-                Task.WaitAll(t1, t2);
-
-                if (r1 == 0 && r2 == 0)
-                    break;
-            }
-            Console.WriteLine($"{count}번 만에 빠져나옴");
         }
     }
 }
