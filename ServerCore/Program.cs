bf515017d398e7acb84f9da24185f2690a76e3c4
joonsooan <joonsooan@gmail.com>
2024-10-21 15:52:20 +0900

    DeadLock

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,5 +1,50 @@
 ﻿namespace ServerCore
 {
+    internal class FastLock
+    {
+        public int id;
+    }
+
+    internal class SessionManager
+    {
+        private static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock (_lock)
+            {
+            }
+        }
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+    }
+
+    internal class UserManager
+    {
+        private static object _lock = new object();
+
+        public static void TestUser()
+        {
+            lock (_lock)
+            {
+            }
+        }
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+    }
+
     internal class Program
     {
         private static int number = 0;
@@ -7,23 +52,17 @@
 
         private static void Thread_1()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 100; i++)
             {
-                lock (_obj)
-                {
-                    number++;
-                }
+                SessionManager.Test();
             }
         }
 
         private static void Thread_2()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 100; i++)
             {
-                lock (_obj)
-                {
-                    number--;
-                }
+                UserManager.Test();
             }
         }
 
@@ -32,6 +71,9 @@
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
             t1.Start();
+
+            Thread.Sleep(100);
+
             t2.Start();
 
             Task.WaitAll(t1, t2);
